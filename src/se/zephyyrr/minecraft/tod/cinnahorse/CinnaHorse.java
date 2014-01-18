bc9d49e9771a28b91ff2409c3ad337f73a75bd9c
zephyyrr <zapmew@gmail.com>
2014-01-18 12:29:06 +0100

    Fixed that newly spawned horses was never stored.

--- src/se/zephyyrr/minecraft/tod/cinnahorse/CinnaHorse.java
+++ src/se/zephyyrr/minecraft/tod/cinnahorse/CinnaHorse.java
@@ -5,12 +5,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
 import org.bukkit.entity.Horse.Variant;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -21,17 +23,20 @@ public class CinnaHorse extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		getLogger().info("Initializing CinnaHorse");
 		horses = new HashMap<Player, Horse>();
 	}
 	
 	@Override
 	public void onDisable() {
+		getLogger().info("Shutting down CinnaHorse");
 		Iterator<Entry<Player, Horse>> iter = horses.entrySet().iterator();
 		while (iter.hasNext()) {
 			Entry<Player, Horse> entry = iter.next();
 			entry.getValue().setHealth(0); //Kill the beasts!
 			iter.remove();
 		}
+		
 	}
 	
 	@Override
@@ -48,28 +53,37 @@ public class CinnaHorse extends JavaPlugin {
 		} else {
 			return false;
 		}
+		
 		if (sender.hasPermission("cinnahorse.summon")) {
-			if (!horses.containsKey(sender))
-				spawnHorse(p);
-			else {
+			if (!horses.containsKey(sender)) {
+				Horse h = spawnHorse(p);
+				horses.put(p, h);
+			} else {
 				horses.get(p).setHealth(0);
 				horses.remove(p);
 			}
+			return true;
+		} else {
+			sender.sendMessage("Sorry, not enough permissions.");
 		}
 		return false;
 	}
 
-	private void spawnHorse(Player p) {
+	private Horse spawnHorse(Player p) {
 		Horse h = (Horse) p.getWorld().spawnEntity(p.getLocation(), EntityType.HORSE);
 		h.setAdult();
-		h.setOwner(p);
-		h.setVariant(Variant.SKELETON_HORSE);
 		h.setJumpStrength(2);
-		h.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2000, 2));
+		h.setVariant(Variant.SKELETON_HORSE);
 		h.setMaxHealth(100);
 		h.setHealth(100);
-		h.setRemoveWhenFarAway(true);
+		
+		h.setOwner(p);
 		h.setCustomName("Shi");
+		h.getEquipment().setChestplate(new ItemStack(Material.SADDLE));
+		h.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2000, 2));
+		h.setRemoveWhenFarAway(true);
+		
+		return h;
 	}
 	
 	@EventHandler
